3830ae016169fba5ede0eab8e829936993229d03
Max Levine <max@maxlevine.co.uk>
2020-12-06 12:28:40 +0000

    Use specific file mount

--- build_run.sh
+++ build_run.sh
@@ -5,6 +5,6 @@ docker build \
   --build-arg USER={$USER} \
   . 
 docker run -it \
-  # -v /home/max/Documents/nextjs/:/run/{$USER}/ \
+  -v /home/max/Documents/nextjs_tutorial/src/pages/index.js:/run/{$USER}/pages/index.js \
   -p 3000:3000 \
   {$USER}
