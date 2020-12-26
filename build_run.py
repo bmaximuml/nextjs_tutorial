18b4a690b3f0dfc37facaaa18d7a1b1eb2519e29
Max Levine <max@maxlevine.co.uk>
2020-12-26 21:29:42 +0000

    Implement --build --run flags

--- build_run.py
+++ build_run.py
@@ -24,136 +24,139 @@ class BuildRun(object):
         api_client = APIClient(base_url='unix://var/run/docker.sock')
         name = f'{user}_{env}'
         tag = f'{user}:{env}'
-        dockerfile = f'Dockerfile.{env}'
-        build_kwargs = {
-            'path': dir,
-            'tag': tag,
-            'dockerfile': dockerfile,
-            'buildargs': {
-                'USER': user,
-            },
-            'nocache': not cache,
-            'pull': pull,
-            'rm': rm,
-            'decode': True,
-        }
-        if verbose >= 2:
-            print('Build Args:')
-            [print(f'\t{key}: {build_kwargs[key]}') for key in build_kwargs.keys()]
 
-        image = api_client.build(**build_kwargs)
-        # image = client.build(**build_kwargs)
-
-        build_success = False
-        if verbose >= 1:
-            print('\nBuild Log:')
-            for i in image:
-                if 'stream' in i:
-                    if 'Successfully built' in i['stream']:
-                        build_success = True
-                    [print(f'\t{line.strip()}') for line in i['stream'].splitlines() if line.strip().strip('\n') != '']
-
-        if not build_success:
-            raise BuildRunError(101, 'Build Failed')
-
-        if env.lower() == 'prod':
-            run_kwargs = {
-                'image': tag,
-                'name': name,
-                'user': user,
-                'ports': {
-                    '3000/tcp': 5000,
+        if build:
+            dockerfile = f'Dockerfile.{env}'
+            build_kwargs = {
+                'path': dir,
+                'tag': tag,
+                'dockerfile': dockerfile,
+                'buildargs': {
+                    'USER': user,
                 },
-                'detach': detach,
+                'nocache': not cache,
+                'pull': pull,
+                'rm': rm,
+                'decode': True,
             }
-        elif env.lower() == 'test':
-            run_kwargs = {
-                'image': tag,
-                'name': name,
-                'user': user,
-                'ports': {
-                    '3000/tcp': 4000,
-                },
-                'detach': detach,
-            }
-        else:
-            run_kwargs = {
-                'image': tag,
-                'name': name,
-                'user': user,
-                'ports': {
-                    '3000/tcp': 3000,
-                },
-                'volumes': {
-                    os.path.join(dir, "src"): {'mode': 'rw', 'bind': os.path.join("/", "run", user)},
-                    # os.path.join(dir, "src", "components"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "components")},
-                    # os.path.join(dir, "src", "lib"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "lib")},
-                    # os.path.join(dir, "src", "pages"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "pages")},
-                    # os.path.join(dir, "src", "posts"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "posts")},
-                    # os.path.join(dir, "src", "public"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "public")},
-                    # os.path.join(dir, "src", "styles"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "styles")},
-                    # os.path.join(dir, "src", "postcss.config.js"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "postcss.config.js")},
-                    # os.path.join(dir, "src", "tailwind.config.js"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "tailwind.config.js")},
-                    # os.path.join(dir, "src", "tsconfig.json"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "tsconfig.json")},
-                    # os.path.join(dir, "src", "package.json"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "package.json")},
-                },
-                'detach': detach,
-            }
+            if verbose >= 2:
+                print('Build Args:')
+                [print(f'\t{key}: {build_kwargs[key]}') for key in build_kwargs.keys()]
 
-        if verbose >= 2:
-            print('\nRun Args:')
-            [print(f'\t{key}: {run_kwargs[key]}') for key in run_kwargs.keys()]
+            image = api_client.build(**build_kwargs)
+            # image = client.build(**build_kwargs)
 
-        all_containers = api_client.containers(
-            all=True,
-            filters={
-                'name': f'^{name}$',
-            },
-        )
-        if all_containers:
+            build_success = False
             if verbose >= 1:
-                print(f'\nContainers exist for {name}...')
-                # [print(f'\t{container["Names"][0].replace("/", "")} : {container["Id"]}') for container in all_containers]
-            for container in all_containers:
-                container_id = container['Id']
-                if container['State'].lower() == 'running':
-                    if verbose >= 1:
-                        print(f'\tStopping running container: {container_id}')
-                    api_client.stop(container_id)
-                rename = f'{name}_{datetime.fromtimestamp(container["Created"])}'.replace(' ', '_').replace(':', '_')
-                count = 1
-                while api_client.containers(all=True, filters={'name': f'^{rename}$'}):
-                    rename = f'{name}_{datetime.fromtimestamp(container["Created"])}_{count}'.replace(' ', '_').replace(':', '_')
-                    count += 1
+                print('\nBuild Log:')
+                for i in image:
+                    if 'stream' in i:
+                        if 'Successfully built' in i['stream']:
+                            build_success = True
+                        [print(f'\t{line.strip()}') for line in i['stream'].splitlines() if line.strip().strip('\n') != '']
 
+            if not build_success:
+                raise BuildRunError(101, 'Build Failed')
+
+        if run:
+            if env.lower() == 'prod':
+                run_kwargs = {
+                    'image': tag,
+                    'name': name,
+                    'user': user,
+                    'ports': {
+                        '3000/tcp': 5000,
+                    },
+                    'detach': detach,
+                }
+            elif env.lower() == 'test':
+                run_kwargs = {
+                    'image': tag,
+                    'name': name,
+                    'user': user,
+                    'ports': {
+                        '3000/tcp': 4000,
+                    },
+                    'detach': detach,
+                }
+            else:
+                run_kwargs = {
+                    'image': tag,
+                    'name': name,
+                    'user': user,
+                    'ports': {
+                        '3000/tcp': 3000,
+                    },
+                    'volumes': {
+                        os.path.join(dir, "src"): {'mode': 'rw', 'bind': os.path.join("/", "run", user)},
+                        # os.path.join(dir, "src", "components"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "components")},
+                        # os.path.join(dir, "src", "lib"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "lib")},
+                        # os.path.join(dir, "src", "pages"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "pages")},
+                        # os.path.join(dir, "src", "posts"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "posts")},
+                        # os.path.join(dir, "src", "public"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "public")},
+                        # os.path.join(dir, "src", "styles"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "styles")},
+                        # os.path.join(dir, "src", "postcss.config.js"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "postcss.config.js")},
+                        # os.path.join(dir, "src", "tailwind.config.js"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "tailwind.config.js")},
+                        # os.path.join(dir, "src", "tsconfig.json"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "tsconfig.json")},
+                        # os.path.join(dir, "src", "package.json"): {'mode': 'ro', 'bind': os.path.join("/", "run", user, "package.json")},
+                    },
+                    'detach': detach,
+                }
+
+            if verbose >= 2:
+                print('\nRun Args:')
+                [print(f'\t{key}: {run_kwargs[key]}') for key in run_kwargs.keys()]
+
+            all_containers = api_client.containers(
+                all=True,
+                filters={
+                    'name': f'^{name}$',
+                },
+            )
+            if all_containers:
                 if verbose >= 1:
-                    print(f'\tRenaming to {rename} (id: {container_id})')
-                api_client.rename(container_id, rename)
-        else:
+                    print(f'\nContainers exist for {name}...')
+                    # [print(f'\t{container["Names"][0].replace("/", "")} : {container["Id"]}') for container in all_containers]
+                for container in all_containers:
+                    container_id = container['Id']
+                    if container['State'].lower() == 'running':
+                        if verbose >= 1:
+                            print(f'\tStopping running container: {container_id}')
+                        api_client.stop(container_id)
+                    rename = f'{name}_{datetime.fromtimestamp(container["Created"])}'.replace(' ', '_').replace(':', '_')
+                    count = 1
+                    while api_client.containers(all=True, filters={'name': f'^{rename}$'}):
+                        rename = f'{name}_{datetime.fromtimestamp(container["Created"])}_{count}'.replace(' ', '_').replace(':', '_')
+                        count += 1
+
+                    if verbose >= 1:
+                        print(f'\tRenaming to {rename} (id: {container_id})')
+                    api_client.rename(container_id, rename)
+            else:
+                if verbose >= 1:
+                    print(f'No existing containers for {name}.')
+
+            container = client.containers.run(**run_kwargs)
             if verbose >= 1:
-                print(f'No existing containers for {name}.')
+                print(f'\nStarted container: {container.id}')
+                print('\nConnect with:')
+                print(f'\tdocker exec -it {name} bash')
+                print('\nView logs with:')
+                print(f'\tdocker logs {name}')
 
-        container = client.containers.run(**run_kwargs)
-        if verbose >= 1:
-            print(f'\nStarted container: {container.id}')
-            print('\nConnect with:')
-            print(f'\tdocker exec -it {name} bash')
-            print('\nView logs with:')
-            print(f'\tdocker logs {name}')
-
-            print(f'Waiting {str(wait)} seconds to ensure container stays up...')
-            run_success = True
-            while wait >= 0:
-                if api_client.containers(filters={'name': f'^{name}$'}):
-                    print(f'\r\t{wait}: Running', end='', flush=True)
-                    wait -= 1
-                    sleep(1)
-                else:
-                    print(f'\r\t{wait}: Died    ', flush=True)
-                    run_success = False
-                    break
-            if run_success:
-                print(f'\r\tSuccess: http://localhost:{run_kwargs["ports"]["3000/tcp"]}/', flush=True)
+                print(f'Waiting {str(wait)} seconds to ensure container stays up...')
+                run_success = True
+                while wait >= 0:
+                    if api_client.containers(filters={'name': f'^{name}$'}):
+                        print(f'\r\t{wait}: Running', end='', flush=True)
+                        wait -= 1
+                        sleep(1)
+                    else:
+                        print(f'\r\t{wait}: Died    ', flush=True)
+                        run_success = False
+                        break
+                if run_success:
+                    print(f'\r\tSuccess: http://localhost:{run_kwargs["ports"]["3000/tcp"]}/', flush=True)
 
 @click.command()
 @click.option('-u', '--user', type=str, default='nextjs', show_default=True, help='User and project name')
